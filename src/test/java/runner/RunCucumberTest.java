3264bae79ca47b2ac7adebf1496fab589826fd4e
kemal <kemal182182@gmail.com>
2025-07-31 00:32:25 +0100

    commit

--- src/test/java/runner/RunCucumberTest.java
+++ src/test/java/runner/RunCucumberTest.java
@@ -15,3 +15,4 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @ConfigurationParameter(key = GLUE_PROPERTY_NAME, value = "stepdefs")
 public class RunCucumberTest {
 }
+
